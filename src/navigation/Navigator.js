39f827698b15c5f78e336b9376a5a15162db24c4
Marius <marius@myidea.fr>
2022-02-21 16:26:12 +0100

    add map to lines screen

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -9,6 +9,7 @@ import HomeScreen from '../screens/HomeScreen'
 import TabBar from './TabBar'
 import ProfileScreen from '../screens/ProfileScreen'
 import LinesScreen from '../screens/LinesScreen'
+import ModalScreen from '../screens/ModalScreen'
 
 const MainNavigator = createBottomTabNavigator()
 
@@ -36,9 +37,14 @@ function MainTabNavigator () {
         headerShown: false
       }}
     >
-      <MainNavigator.Screen name='Home' options={{ tabBarIcon: 'home' }} component={HomeScreen} />
-      <MainNavigator.Screen name='Lines' options={{ tabBarIcon: 'analytics' }} component={LinesScreen} />
-      <MainNavigator.Screen name='Profile' options={{ tabBarIcon: 'person' }} component={ProfileScreen} />
+      <MainNavigator.Group>
+        <MainNavigator.Screen name='Home' options={{ tabBarIcon: 'home' }} component={HomeScreen} />
+        <MainNavigator.Screen name='Lines' options={{ tabBarIcon: 'analytics' }} component={LinesScreen} />
+        <MainNavigator.Screen name='Profile' options={{ tabBarIcon: 'person' }} component={ProfileScreen} />
+      </MainNavigator.Group>
+      <MainNavigator.Group screenOptions={{ presentation: 'modal' }}>
+        <MainNavigator.Screen name='Modal' component={ModalScreen} />
+      </MainNavigator.Group>
     </MainNavigator.Navigator>
   )
 }
--- src/screens/LinesScreen.js
+++ src/screens/LinesScreen.js
@@ -1,9 +1,59 @@
-import React from 'react'
-import { Text } from 'native-base'
+import React, { useEffect, useState } from 'react'
+import { Container, Fab, Text } from 'native-base'
+import Icon from 'react-native-vector-icons/Ionicons'
+import MapView from 'react-native-maps'
+import { PermissionsAndroid } from 'react-native'
 
-function LinesScreen () {
+function LinesScreen ({ navigation }) {
+  const [mapMargin, setMapMargin] = useState(1)
+  const [mapPaddingTop, setMapPaddingTop] = useState()
+  useEffect(() => {
+    setTimeout(() => {
+      setMapMargin(1)
+      setMapMargin(1)
+    }, 100)
+  }, [])
+  const onMapReady = () => {
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+      .then((granted) => {
+        setMapMargin(0)
+        setMapMargin(0)
+      })
+      .finally(() => {
+        setMapMargin(0)
+        setMapPaddingTop(0)
+      })
+
+    setMapMargin(0)
+    setMapPaddingTop(0)
+  }
+
   return (
-    <Text>LINES SCREEN</Text>
+    <Container style={{ maxWidth: '100%', paddingTop: mapPaddingTop }} h='100%' w='100%'>
+      <MapView
+        provider='google'
+        style={{
+          flex: 1,
+          width: '100%',
+          height: '100%',
+          marginBottom: mapMargin
+        }}
+        zoomControlEnabled
+        showsUserLocation
+        showsMyLocationButton
+        showsCompass
+        showsScale
+        onMapReady={onMapReady}
+      />
+      <Fab
+        onPress={() => navigation.navigate('Modal')}
+        position='absolute'
+        bottom={90}
+        right={5}
+        size='md'
+        icon={<Icon name='add' size={25} />}
+      />
+    </Container>
   )
 }
 
