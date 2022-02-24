37515413ab9390a1978eb98c841f50266565efba
Cyrielle Istin/Cycycode <cycycode@gmail.com>
2022-02-24 15:39:01 +0100

    add a toggle to handle trips

--- src/components/profile/UserTrips.js
+++ src/components/profile/UserTrips.js
@@ -1,19 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Text,
   Container,
   VStack,
-  Switch
+  Switch,
+  Center,
+  Box,
+  Heading,
+  ScrollView
 } from 'native-base'
 
-function UserTrips () {
+function UserTrips() {
+  // state pour switcher trajets passés ou à venir
+  const [toggleTrips, setToggleTrips] = useState(true)
+
+  // fonction pour le boléen du toggle
+  const handleTrips = () => {
+    setToggleTrips(!toggleTrips)
+  }
+
   return (
-    <Container>
-      <Text> USER TRIPS COMPONENT</Text>
-      <VStack space={4} alignItems='center'>
-        <Switch size='md' />
-      </VStack>
-    </Container>
+    <Center>
+      <Container>
+        <Box flexDirection={'row'} h="70px" alignItems="center" alignSelf="center" marginBottom="8">
+          <Text>Trajets passés</Text>
+          <VStack space={4}>
+            <Switch isChecked={toggleTrips} size="md" onToggle={handleTrips} />
+          </VStack>
+          <Text>Trajets à venir</Text>
+        </Box>
+        <ScrollView maxW="100%" h="60%" _contentContainerStyle={{
+          px: "20px",
+          mb: "4",
+          minW: "72"
+        }}>
+          <Center mt="2" mb="8">
+            <Heading fontSize="xl">Mes trajets</Heading>
+          </Center>
+          <VStack flex="1">
+            <Box h="80px">
+              <Text>Mon premier trajet</Text>
+            </Box>
+            <Box h="80px">
+              <Text>Mon deuxième trajet</Text>
+            </Box>
+            <Box h="80px">
+              <Text>Mon troisième trajet</Text>
+            </Box>
+          </VStack>
+        </ScrollView>
+      </Container>
+    </Center>
   )
 }
 
